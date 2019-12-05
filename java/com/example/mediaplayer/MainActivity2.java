7bf725eb75cc06fe7729c39662fe629157c85a7a
Enockkk <44627709+Enockkk@users.noreply.github.com>
2019-12-05 21:24:24 +0800

    Add files via upload

---
+++ java/com/example/mediaplayer/MainActivity2.java
@@ -0,0 +1,56 @@
+package com.example.mediaplayer;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.mediaplayer.R;
+
+import java.io.IOException;
+
+public class MainActivity2 extends AppCompatActivity {
+    String[] filePathList2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        ListView lv = (ListView) findViewById(R.id.lv);
+
+        AssetManager assetManager2 = getAssets();
+        try {
+            filePathList2 = assetManager2.list("");
+        } catch (IOException e) {
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.item,R.id.tv_name,filePathList2);
+        lv.setAdapter(adapter);
+
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity2.this);
+                builder.setTitle("是否添加歌曲");
+                builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent=new Intent(MainActivity2.this, com.example.mediaplayer.MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                builder.setNegativeButton("取消", null);
+                builder.create();
+                builder.show();
+                return false;
+            }
+
+        });
+    }
+}
